11f02a108267059fa3e97b06fe42d7dc5159962d
Trey <tajohns22@gmail.com>
2020-12-07 13:04:33 -0600

    updated express require

--- mydroplist-backend/app.js
+++ mydroplist-backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const ExpressError = require('./helpers/ExpressError');
+const ExpressError = require('./helpers/expressError');
 const {authenticateJWT, droplistAccess} = require('./middleware/auth')
 
 const authRoutes = require('./routes/auth');
