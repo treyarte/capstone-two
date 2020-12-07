f52c31049b77471b71e8cee10d98e3d7ebcd441d
Trey <tajohns22@gmail.com>
2020-12-07 13:39:53 -0600

    updated file name ExpressErrors

--- mydroplist-backend/app.js
+++ mydroplist-backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const ExpressError = require('./helpers/expressError');
+const ExpressError = require('./helpers/ExpressError');
 const {authenticateJWT, droplistAccess} = require('./middleware/auth')
 
 const authRoutes = require('./routes/auth');
