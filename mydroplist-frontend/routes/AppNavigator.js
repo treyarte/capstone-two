c59dbd53f0e302ba45bdd852f7891209d0aab154
Trey <tajohns22@gmail.com>
2021-01-16 17:44:07 -0600

    starte tab navigation

--- mydroplist-frontend/routes/AppNavigator.js
+++ mydroplist-frontend/routes/AppNavigator.js
@@ -1,17 +1,24 @@
 import React, {useContext} from 'react';
 import {NavigationContainer} from '@react-navigation/native'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import SignedOutStack from './SignedOutStack';
 import SignedInStack from './SignedInStack';
 import {TokenContext} from '../components/tokenContext'
+import DroplistIndex from '../screens/DroplistIndex';
 
 const AppNavigator = () => {
 
     const [token] = useContext(TokenContext);
-
+    const Tab = createBottomTabNavigator();
     return (
         <NavigationContainer>
         {token !== null ? (
-            <SignedInStack/>
+            <>
+                <SignedInStack/>
+                <Tab.Navigator>
+                    <Tab.Screen name='DroplistIndex' component={DroplistIndex}/>
+                </Tab.Navigator>
+            </>
         ) : (
             <SignedOutStack />
         )}
